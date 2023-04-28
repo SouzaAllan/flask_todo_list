9104bd36429b8dcea919940e2d0776a35132c0f2
souzaallan <allanfeitosa@hotmail.com>
2023-04-28 00:44:44 -0300

    inclusao-novas-paginas

--- main.py
+++ main.py
@@ -3,7 +3,25 @@ from flask import Flask, render_template
 app = Flask(__name__,template_folder='template')
 
 @app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/cadastro')
 def form():
     return render_template('form.html')
+
+@app.route('/pos_cadastro')
+def pos_cadastro():
+    return render_template('pos_cadastro.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/principal')
+def principal():
+    return render_template('principal.html')
+
+
 if __name__ == '__main__':
     app.run()
