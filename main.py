47bc57f603066dec50ae7fd1e1256132be45e388
souzaallan <allanfeitosa@hotmail.com>
2023-05-08 02:16:28 -0300

    last-version

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import json
 import requests as rs
+import time
 
 app = Flask(__name__,template_folder='template')
 
@@ -125,9 +126,157 @@ def submit_update():
 
 
 
-@app.route('/tasks')
-def task():
-    return render_template('tasks.html')
+@app.route('/tasks',methods=['POST'])
+def tasks():
+    token  = request.form['token']
+    print(token)
+    return render_template('tasks.html', token=token)
+
+@app.route('/create_task',methods=['POST'])
+def create_tasks():
+    token  = request.form['token']
+    return render_template('create_task.html', token=token)
+
+@app.route('/submit_create_task',methods=['POST'])
+def submit_create_task():
+    token  = request.form['token']
+    name  = request.form['name']
+    url = f"https://todolist-api.edsonmelo.com.br/api/task/new/"
+
+    # Corpo da mensagem a ser enviada para a API com as informações
+    # Veja na documentação as opções de pay_load de acordo com o recurso e serviço desejado
+    pay_load = {
+        'name' : name
+        }
+
+    # Cabeçalho da requisição informando o que deverá ser enviado e qual o formato
+    headers = {'Content-type': 'application/json', 'Authorization': token}
+
+        # Envio da requisição e armazenamento dos dados recebidos
+    data_update = rs.post(url, data=json.dumps(pay_load), headers=headers)
+    # Converte os dados recebidos em em dicionário Python
+    print(data_update.text)
+    dicionario = json.loads(data_update.text)
+    print(dicionario)
+    specific_key = "message"
+    if isinstance(dicionario, dict) and specific_key in dicionario and isinstance(dicionario[specific_key], str) and dicionario[specific_key] == "Task Successfully Added":
+
+        return render_template('submit_create_task.html')
+    else:
+        # Mostra os dados retornados já convertidos
+        return 'deu problema ai'
+
+
+@app.route('/see_tasks',methods=['POST'])
+def see_tasks():
+    time.sleep(3)
+    token  = request.form['token']
+    '''url = f"https://todolist-api.edsonmelo.com.br/api/task/search/"'''
+
+    recurso = 'task'    # Resources (veja na documentação)
+    servico = 'search'   # Cada resource tem seu métodos para os seviços
+    url = f"https://todolist-api.edsonmelo.com.br/api/{recurso}/{servico}/"
+
+    # Corpo da mensagem a ser enviada para a API com as informações
+    # Veja na documentação as opções de pay_load de acordo com o recurso e serviço desejado
+    '''pay_load = {
+        'id' : 'jogar'
+        }'''
+
+    # Cabeçalho da requisição informando o que deverá ser enviado e qual o formato
+    headers = {'Content-type': 'application/json', 'Authorization': token}
+
+    # Envio da requisição e armazenamento dos dados recebidos
+    dados = rs.post(url,  headers=headers)
+
+    # parse JSON response into a dictionary
+    dd = json.loads(dados.text)
+    data = json.loads(dados.content)
+
+    # Cabeçalho da requisição informando o que deverá ser enviado e qual o formato
+    headers = {'Content-type': 'application/json', 'Authorization': token}
+
+    # Envio da requisição e armazenamento dos dados recebidos
+    '''dados = rs.post(url,  headers=headers)
+    print(dados.text)
+    dicionario = json.loads(dados.text)
+    data= json.loads(dados.content)'''
+    
+    
+    if 'message' in dd:
+        # Gera uma mensagem de erro com o valor retornado pela API ou conexão
+        return render_template('sem_tasks.html')
+        
+    else:
+        # Mostra os dados retornados já convertidos
+        
+        list_dict = [{'id': item['id'],'name': item['name'], 'realized': item['realized']} for item in data]
+
+        return render_template('see_task.html', tasks=list_dict, token=token)
+
+@app.route('/delete_tasks',methods=['POST'])
+def delete_tasks():
+    token  = request.form['token']
+    return render_template('delete_task.html',token=token)
+
+@app.route('/submit_delete_task',methods=['POST'])
+def delete_tasks_submit():
+    token  = request.form['token']
+    id  = request.form['id']
+    url = f"https://todolist-api.edsonmelo.com.br/api/task/delete/"
+    # Cabeçalho da requisição informando o que deverá ser enviado e qual o formato
+    headers = {'Content-type': 'application/json', 'Authorization': token}
+    # send data to external API
+    data_log = {
+
+        'id': id
+    }
+    
+    dados_delete = rs.delete(url, data=json.dumps(data_log), headers=headers)
+    # Converte os dados recebidos em em dicionário Python
+    dicionario = json.loads(dados_delete.text)
+    
+    specific_key = "message"
+    if isinstance(dicionario, dict) and specific_key in dicionario and isinstance(dicionario[specific_key], str) and dicionario[specific_key] != "Task deleted Successfully":
+
+        return render_template('id_task_invalido.html',token = token)
+    else:
+        # Mostra os dados retornados já convertidos
+        return render_template('submit_delete_task.html',token=token)
+
+@app.route('/update_tasks', methods=['POST'])
+def update_task():
+    token  = request.form['token']
+    return render_template('update_task.html',token=token)
+
+
+@app.route('/submit_update_task', methods=['POST'])
+def submit_update_task():
+    token  = request.form['token']
+    id  = request.form['id']
+    name = request.form['name']
+    realized = request.form['realized']
+    url = f"https://todolist-api.edsonmelo.com.br/api/task/update/"
+    # Cabeçalho da requisição informando o que deverá ser enviado e qual o formato
+    headers = {'Content-type': 'application/json', 'Authorization': token}
+    # send data to external API
+    data_log = {
+
+        'id': id,
+        'name' : name,
+        'realized': realized
+    }
+    
+    dados = rs.put(url, data=json.dumps(data_log), headers=headers)
+    # Converte os dados recebidos em em dicionário Python
+    dicionario = json.loads(dados.text)
+    specific_key = "message"
+    if isinstance(dicionario, dict) and specific_key in dicionario and isinstance(dicionario[specific_key], str) and dicionario[specific_key] != "Task Successfully Updated":
+
+        return render_template('id_task_invalido.html',token = token)
+    else: 
+        return render_template('submit_update_task.html',token = token)
+    
 
 @app.route('/delete', methods=['POST'])
 def delete():
